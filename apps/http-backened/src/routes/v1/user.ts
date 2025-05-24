7719a3dac3b350460edf7d0dfb1f7146d588ddb5
sahil kumar <sahilgorka123@gmail.com>
2025-05-24 10:33:58 +0530

    added signup endpoints db calls

--- apps/http-backened/src/routes/v1/user.ts
+++ apps/http-backened/src/routes/v1/user.ts
@@ -1,20 +1,63 @@
-import { generateToken } from 'authenticator';
+import { generateToken, verifyToken } from 'authenticator';
 import {Router} from 'express';
+import {client} from '@repo/db/client'
+import jwt from 'jsonwebtoken';
+import { SECRET } from '../../config';
 
 const userRouter:Router = Router();
 
 userRouter.post("/signup", (req, res)=> {
-        const phoneNumber = req.body;
-        const totp = generateToken(phoneNumber + "SIGNUP");
+        const number = req.body;
+        const totp = generateToken(number + "SIGNUP");
 
+      const user =  client.user.upsert({
+        where:{
+            number,
+            name: ''
+        },
+        create:{
+            number,
+            name: ''
+        },
+        update:{
+        
+        }
+    
+           
+        })
         res.json({
             totp
         })
 });
 
 
-userRouter.post('/signup/verify',(req,res) => {
+userRouter.post('/signup/verify',async(req,res) => {
+    const number = req.body.phoneNumber;
+    const name = req.body.name;
+    if(!verifyToken(number + "SIGNUP", req.body.otp)) {
+        res.json({
+            message: "Invalid token"
+        })
+        return
+    }
 
+    const userId = await client.user.update({
+        where:{
+            number
+        },
+        data:{
+            name,
+            verified:true
+        }
+    })
+
+    const token = jwt.sign({
+        userId
+    }, SECRET)
+
+    res.json(
+        token
+    )
 })
 
 export {userRouter}
