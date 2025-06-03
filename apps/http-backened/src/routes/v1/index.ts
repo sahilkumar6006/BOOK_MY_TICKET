68025ebcb816e73ce04dd33364bbb9d5b9fdf9ae
sahil kumar <sahilgorka123@gmail.com>
2025-06-03 17:05:19 +0530

    added teh Bookmy Ticket changes

--- apps/http-backened/src/routes/v1/index.ts
+++ apps/http-backened/src/routes/v1/index.ts
@@ -2,6 +2,13 @@ import {Router} from 'express';
 import {userRouter} from './user'
 const mainRouter: Router = Router();
 
-mainRouter.use('/user', userRouter)
+mainRouter.use('/user', userRouter);
+
+
+if (process.env.NODE_ENV !== "production") {
+    // Used only for testing, should never be deployed to prod.
+    // Lets the tester create admins etc
+    mainRouter.use("/test", testRouter);
+}
 
 export {mainRouter}
