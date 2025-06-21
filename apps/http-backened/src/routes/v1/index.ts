822797db7673f15c346641e23237d3216a8f100d
sahil kumar <sahilgorka123@gmail.com>
2025-06-21 10:00:05 +0530

    initial setup till signup

--- apps/http-backened/src/routes/v1/index.ts
+++ apps/http-backened/src/routes/v1/index.ts
@@ -7,8 +7,6 @@ mainRouter.use('/user', userRouter);
 
 
 if (process.env.NODE_ENV !== "production") {
-    // Used only for testing, should never be deployed to prod.
-    // Lets the tester create admins etc
     mainRouter.use("/test", TestRouter);
 }
 
